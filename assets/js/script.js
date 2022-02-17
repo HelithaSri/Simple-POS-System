7ef107d20307e600be74233a57a483494003fee0
HelithaSri <helitha.pravin@gmail.com>
2022-02-17 12:27:54 +0530

    Clear Input Fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// document.getElementById("home").style.setProperty("Display","none","important");
+document.getElementById("home").style.setProperty("Display","none","important");
 document.getElementById("order").style.setProperty("Display","none","important");
 document.getElementById("item").style.setProperty("Display","none","important");
-document.getElementById("cutomer").style.setProperty("Display","none","important");
+//document.getElementById("cutomer").style.setProperty("Display","none","important");
 
 // NavBar clicks
 document.getElementById("home-click").addEventListener("click",function (){
@@ -68,3 +68,111 @@ document.getElementById("item-clicks").addEventListener("click",function (){
     document.getElementById("home").style.setProperty("Display","none","important");
     document.getElementById("order").style.setProperty("Display","none","important");
 });
+
+//-------------------------------------------------------------------------------------
+
+/* Customer Section Js - Start */
+
+/* Validation - Start */
+$('#error1').css({ "display": "none" });
+$('#error2').css({ "display": "none" });
+$('#error3').css({ "display": "none" });
+$('#error4').css({ "display": "none" });
+
+$('#error01').css({ "display": "none" });
+$('#error02').css({ "display": "none" });
+$('#error03').css({ "display": "none" });
+$('#error04').css({ "display": "none" });
+
+var regExCusID = /^(C00-)[0-9]{3,4}$/;
+var RegExCusName = /^[A-z]{5,10}$/;
+var RegExCusAddress = /^(No.)[0-9]{2} [A-z]{4,100}$/;
+var RegExCusTP = /^(07)[0-9]{8,9}$/;
+
+// Customer Validation Function Call - Start
+
+validation(regExCusID, '#cusIdAdd', '#error1', '#cusNameAdd', '#btnAddCus');
+validation(RegExCusName, '#cusNameAdd', '#error2', '#cusAddressAdd', '#btnAddCus');
+validation(RegExCusAddress, '#cusAddressAdd', '#error3', '#cusSalaryAdd', '#btnAddCus');
+validation(RegExCusTP, '#cusSalaryAdd', '#error4', "#btnAddCus", '#btnAddCus');
+
+validation(regExCusID, '#cusIdUpdate', '#error01', "#cusNameUpdate", '#btnUpdateCus');
+validation(RegExCusName, '#cusNameUpdate', '#error02', '#cusAddressUpdate', '#btnUpdateCus');
+validation(RegExCusAddress, '#cusAddressUpdate', '#error03', '#cusSalaryUpdate', '#btnUpdateCus');
+validation(RegExCusTP, '#cusSalaryUpdate', '#error04', '#btnUpdateCus', '#btnUpdateCus');
+
+// Customer Validation Function Call - End
+
+addCustomer();
+
+// Customer Validation Function - Start
+
+function validation(regEx, id, error, nextId, btn) {
+
+    $(id).keyup(function () {
+        let input = $(id).val();
+        if (!regEx.test(input)) {
+            if ($(id).val() != '') {
+                $(id).css({ 'border': '2px solid red', 'background-color': '#ffe6e6' });
+                $(error).css({ "color": "red", "display": "block" });
+                $(btn).prop('disabled', true);
+            }
+        }
+        else {
+            $(id).css({ 'border': '2px solid green', 'background-color': '#fff' });
+            $(error).css({ "display": "none" });
+            $(id).keyup(function (event) {
+                console.log(event);
+                if (event.key == "Enter") {
+                    $(btn).prop('disabled', false);
+                    $(nextId).focus();
+                }
+            });
+        }
+    });
+
+}
+
+// Customer Validation Function - End
+
+/* Validation - Start */
+
+// Customer Add Function - Start
+
+function addCustomer() {
+    $("#btnAddCus").click(function () {
+        $("#cusTblBody > tr").off();
+        alert("d");
+        let custId = $("#cusIdAdd").val();
+        let custName = $("#cusNameAdd").val();
+        let custAddress = $("#cusAddressAdd").val();
+        let custTP = $("#cusSalaryAdd").val();
+
+        let btns =
+            "<button class='btn btn-warning' data-bs-target='#updateCustomer' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button class='btn btn-danger'><i class='bi bi-trash'></i></button>";
+
+        let nRow =
+            "<tr><th>" +
+            custId +
+            "</th><td>" +
+            custName +
+            "</td><td>" +
+            custAddress +
+            "</td><td>" +
+            custTP +
+            "</td><td class='text-center'>" +
+            btns +
+            "</td></tr>";
+
+        $("#cusTblBody").append(nRow);
+
+        // Clear input Fields
+        $("#cusIdAdd,#cusNameAdd,#cusAddressAdd,#cusSalaryAdd").val("");
+
+    });
+
+}
+
+// Customer Add Function - End
+
+/* Customer Section Js - End */
